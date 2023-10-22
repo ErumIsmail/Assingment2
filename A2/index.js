aa5e61e9b5f8aa6ec164d04b8c5df0c9dd457e20
Erum Ismail <143540274+ErumIsmail@users.noreply.github.com>
2023-10-22 15:20:26 +0500

    Add files via upload

---
+++ A2/index.js
@@ -0,0 +1,5 @@
+let userInput = prompt("Enter ANy String");
+let trimmedInput = userInput.replace(/\s/g,'');
+document.write(`<h1>The Orignal String is ${userInput}<h1>`);
+document.write(`<h1>The Trimmed  String is ${trimmedInput}<h1>`);
+
